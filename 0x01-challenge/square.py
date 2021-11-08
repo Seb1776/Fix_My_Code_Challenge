966d986285a59331d6b853de044fa20cbb921869
Seb1776 <sebastianwho1776@gmail.com>
2021-11-08 06:07:35 +0000

    Corrected square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -16,10 +16,12 @@ class Square():
         """ Area of the square """
         return self.width * self.height
 
-    def perimeter_of_my_square(self):
+    def permiter_of_my_square(self):
+        """ Returns de Perimeter of the Square """
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ Definition of the Square """
         return "{}/{}".format(self.width, self.height)
 
 
@@ -28,4 +30,4 @@ if __name__ == "__main__":
     s = Square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.perimeter_of_my_square())
+    print(s.permiter_of_my_square())
